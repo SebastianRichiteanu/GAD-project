39d40866cabc206011d1a6d369a5096b63c1a3e9
Richițeanu Mihai-Sebastian <richiteanu.sebastian@gmail.com>
2021-07-30 13:32:49 +0300

    addedd all models + migrations

--- record_label_project/record_label/models.py
+++ record_label_project/record_label/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MyModel(models.Model):
+    class Meta:
+        abstract = True
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Artist(MyModel):
+    class Meta:
+        db_table: 'record_label_artist'
+    first_name = models.CharField(max_length=255, unique=False)
+    last_name = models.CharField(max_length=255, unique=False)
+    name = models.CharField(max_length=255, unique=True)
+    sex = models.CharField(max_length=10, unique=False)
+    phone = models.CharField(max_length=15, unique=True)
+    mail = models.CharField(max_length=255, unique=True)
+
+
+class Album(MyModel):
+    class Meta:
+        db_table: 'record_label_album'
+    name = models.CharField(max_length=255, unique=False)
+    no_songs = models.IntegerField()
+
+
+class Song(MyModel):
+    class Meta:
+        db_table: 'record_label_song'
+    name = models.CharField(max_length=255, unique=False)
+    id_artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    id_album = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True)
+    publish_date = models.DateTimeField()
+
+
+class Collaboration(MyModel):
+    class Meta:
+        db_table: 'record_label_collaboration'
+        unique_together = (("id_song", "id_artist"),)
+    id_song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    id_artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Location(MyModel):
+    class Meta:
+        db_table: 'record_label_location'
+
+    country = models.CharField(max_length=255, unique=False)
+    city = models.CharField(max_length=255, unique=False)
+    street = models.CharField(max_length=255, unique=False)
+    name = models.CharField(max_length=255, unique=False)
+
+
+class Concert(MyModel):
+    class Meta:
+        db_table: 'record_label_concert'
+
+    id_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    publish_date = models.DateTimeField()
+
+
+class Contract(MyModel):
+    class Meta:
+        db_table: 'record_label_contract'
+        unique_together = (("id_artist", "id_concert"),)
+
+    id_artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    id_concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
